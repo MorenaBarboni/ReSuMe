9f1d9401486844067535c41fe38153d30d1c8195
FRANCESCO CASONI <francesco01.casoni@studenti.unicam.it>
2021-08-07 15:25:00 +0200

    results, added reg. tests dependencies, rm content

--- example_content/contracts/ConditionalCoupon.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity ^0.5.2;
-import '@gnosis.pm/hg-contracts/contracts/PredictionMarketSystem.sol';
-
-contract ConditionalCoupon is PredictionMarketSystem {
-
-}
--- example_content/contracts/ContractProvider.sol
+++
@@ -1,5 +0,0 @@
-pragma solidity ^0.5.2;
-
-contract ContractProvider {
-    function contracts(bytes32 contractName) public returns (address addr);
-}
